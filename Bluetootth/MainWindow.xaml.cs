31bbdb6af896adcf125c05268f7493bb19547d78
Jana is <123972079+janais9@users.noreply.github.com>
2025-12-24 23:19:31 +0300

    Upload Files

---
+++ Bluetootth/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using InTheHand.Net.Sockets;
+using InTheHand.Net.Bluetooth;
+
+namespace Bluetootth
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            DevicesList.Items.Clear();
+
+            BluetoothClient client = new BluetoothClient();
+
+            var devices = client.DiscoverDevices();
+
+            foreach (var device in devices)
+            {
+                DevicesList.Items.Add(
+                    $"{device.DeviceName} | {device.DeviceAddress}"
+                );
+            }
+
+            if (!devices.Any())
+            {
+                DevicesList.Items.Add("No Bluetooth devices found.");
+            }
+        }
+    }
+}
